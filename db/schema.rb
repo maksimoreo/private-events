a8f04490586a7fd6fc6dba43c2c99f46e61aaee7
maksim <maximthebest@outlook.com>
2020-12-26 13:25:08 +0200

    UsersController

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_102937) do
+ActiveRecord::Schema.define(version: 2020_12_26_103244) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "info"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
